cd01cd27c5f99210380553cc1e1c360cba0e5476
Chance Neff <crneff84@gmail.com>
2016-09-09 08:30:05 -0700

    initial commit for event planner program

---
+++ src/test/java/EventTest.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class EventTest {
+  import org.junit.*;
+  import static org.junit.Assert.*;
+
+  public class EventTest {
+
+    @Test
+    public void runPingPong_countUpToOne_ArrayList() {
+      // contents of test will eventually go here. 
+    }
+  }
+}
