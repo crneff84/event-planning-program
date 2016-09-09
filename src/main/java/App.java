89afaba7a526dc2ba1ce2793b8a46ef13b6a3131
Chance Neff <crneff84@gmail.com>
2016-09-09 11:47:38 -0700

    update output messages and include backend for showing user choices

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,19 +7,22 @@ public class App {
     System.out.println("How many guests will be attending?");
     String stringGuests = myConsole.readLine();
 
-    System.out.println("What type of meal would you like to provide? Enter 1 for Chicken or 2 for Steak.");
+    System.out.println("What type of meal would you like to provide?" + '\n' + "Enter 1 for Chicken or 2 for Steak.");
     String stringMeal = myConsole.readLine();
 
-    System.out.println("What type of beverages would you like to provide? Enter 1 for alcoholic or 2 for non-alcoholic.");
+    System.out.println("What type of beverages would you like to provide?" + '\n' + "Enter 1 for alcoholic or 2 for non-alcoholic.");
     String stringBeverage = myConsole.readLine();
 
-    System.out.println("What type of entertainment would you like to provide? Enter 1 for a live band or 2 for a DJ");
+    System.out.println("What type of entertainment would you like to provide?" + '\n' + "Enter 1 for a live band or 2 for a DJ");
     String stringEntertainment = myConsole.readLine();
 
     Event name = new Event(Integer.parseInt(stringGuests), Integer.parseInt(stringMeal), Integer.parseInt(stringBeverage), Integer.parseInt(stringEntertainment));
 
     name.calculateCost();
-    name.showChoices();
-    System.out.println("The total cost of your event is " + "$"+ name.getTotal() + ".00");
+    name.showMeal();
+    name.showBeverage();
+    name.showEntertainment();
+    System.out.println('\n' + "INVOICE" + '\n' + "Requested number of guests: " + name.getGuests() + '\n' + "Your choices are:" + '\n' + "Meal: " + name.getMealChoice() + '\n' + "Beverages: " + name.getBeverageChoice() + '\n' + "Entertainment: " + name.getEntertainmentChoice());
+    System.out.println("The estimated total cost of your event is " + "$"+ name.getTotal() + ".00" + '\n');
   }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -4,6 +4,10 @@ class Event {
   private Integer mBeverage;
   private Integer mEntertainment;
   private Integer total = 0;
+  private String mealChoice;
+  private String beverageChoice;
+  private String entertainmentChoice;
+
 
   public Event(Integer guests, Integer meal, Integer beverage, Integer entertainment) {
     mGuests = guests;
@@ -16,17 +20,37 @@ class Event {
     return total;
   }
 
+  public Integer getGuests() {
+    return mGuests;
+  }
+
+  public String getMealChoice() {
+    return mealChoice;
+  }
+
+  public String getBeverageChoice() {
+    return beverageChoice;
+  }
+
+  public String getEntertainmentChoice() {
+    return entertainmentChoice;
+  }
+
   public Integer calculateCost() {
 
     if (mMeal == 1) {
       total += 10 * mGuests;
     } else if (mMeal == 2) {
       total += 15 * mGuests;
+    } else {
+      total += 10 * mGuests;
     }
 
     if (mBeverage == 1) {
       total += 10 * mGuests;
     } else if (mBeverage == 2) {
+      total += 5 * mGuests;
+    } else {
       total += 5 * mGuests;
     }
 
@@ -34,10 +58,42 @@ class Event {
       total += 1000;
     } else if (mEntertainment == 2) {
       total += 500;
+    } else {
+      total += 500;
     }
-
     return total;
   }
 
-  
+  public String showMeal() {
+    if (mMeal == 2) {
+      mealChoice = "Steak";
+    } else if (mMeal == 1) {
+      mealChoice = "Chicken";
+    } else {
+      mealChoice = "Your selection is not available. The standard package includes Chicken as a meal.";
+    }
+    return mealChoice;
+  }
+
+  public String showBeverage() {
+    if (mBeverage == 1) {
+      beverageChoice = "Alcoholic Beverages";
+    } else if (mBeverage == 2) {
+      beverageChoice = "Non-Alcoholic Beverages";
+    } else {
+      beverageChoice = "Your selection is not available. The standard package includes non-alcoholic beverages.";
+    }
+    return beverageChoice;
+  }
+
+  public String showEntertainment() {
+    if (mEntertainment == 1) {
+      entertainmentChoice = "Live Band";
+    } else if (mEntertainment == 2) {
+      entertainmentChoice = "DJ";
+    } else {
+      entertainmentChoice = "Your selection is not available. The standard package includes a DJ for entertainment.";
+    }
+    return entertainmentChoice;
+  }
 }
